8c655f422fdc56573605e7d3a7233289aafc95ab
Rahulchapali <rickyrahul695@gmail.com>
2025-01-23 19:04:50 +0530

    Add Two numbers LEET CODE 005

---
+++ LTcode004.java
@@ -0,0 +1,62 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+            int total = val1 + val2 + carry;
+
+            carry = total / 10;
+            current.next = new ListNode(total % 10);
+            current = current.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return dummy.next;
+    }
+}
+
+public class LTcode004 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        ListNode l1 = createLinkedList(new int[]{2, 4, 3});
+        ListNode l2 = createLinkedList(new int[]{5, 6, 4});
+        ListNode result = solution.addTwoNumbers(l1, l2);
+
+        printLinkedList(result);
+    }
+
+    private static ListNode createLinkedList(int[] values) {
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+        for (int value : values) {
+            current.next = new ListNode(value);
+            current = current.next;
+        }
+        return dummy.next;
+    }
+
+    private static void printLinkedList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val);
+            if (node.next != null) System.out.print(" -> ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
